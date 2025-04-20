e4530cd1f19742e865b57ce8f4e4593678f8f16b
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-21 09:20:59 +1200

    test

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
+import { ColorModeButton } from '@/components/ui/color-mode'
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize='60px' />
       <Text>NavBar</Text>
+      <ColorModeButton />
     </HStack>
   )
 }
