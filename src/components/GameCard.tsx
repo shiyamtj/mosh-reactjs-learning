697e8f81ca208f29815a5cc46db4ba2cd4a49609
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-21 16:43:08 +1200

    score

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, HStack, Image } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList'
+import CriticScore from './CriticScore'
 
 interface GameCardProps {
   game: Game
@@ -8,13 +9,17 @@ interface GameCardProps {
 
 const GameCard = ({ game }: GameCardProps) => {
   return (
-    <Card borderRadius={10} overflow={'hidden'}>
+    <Card borderRadius={10} overflow='hidden'>
       <Image src={game.background_image} />
       <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <PlatformIconList
-          platforms={game.parent_platforms.map((p) => p.platform)}
-        />
+        <Heading fontSize='2xl'>{game.name}</Heading>
+
+        <HStack justifyContent='space-between'>
+          <PlatformIconList
+            platforms={game.parent_platforms.map((p) => p.platform)}
+          />
+          <CriticScore score={game.metacritic} />
+        </HStack>
       </CardBody>
     </Card>
   )
