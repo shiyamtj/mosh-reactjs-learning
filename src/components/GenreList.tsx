c7072c9fef7a0898953b88a106fc7a09a930a602
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-22 19:50:22 +1200

    cleanup genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -6,6 +6,7 @@ import {
   Text,
   Spinner,
   Button,
+  Heading,
 } from '@chakra-ui/react'
 import useGenres, { Genre } from '../hooks/useGenre'
 import getCroppedImageUrl from '../services/image-url'
@@ -22,29 +23,37 @@ const GenreList = ({ onSelectGenre, selectedGenre }: GenreListProps) => {
   if (isLoading) return <Spinner />
 
   return (
-    <List>
-      {data.map((genre) => (
-        <ListItem key={genre.id} paddingY='5px'>
-          <HStack>
-            <Image
-              boxSize='32px'
-              borderRadius={8}
-              src={getCroppedImageUrl(genre.image_background)}
-            />
-            <Button
-              fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
-              fontSize='lg'
-              variant='link'
-              onClick={() => {
-                onSelectGenre(genre)
-              }}
-            >
-              {genre.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize='2xl' marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {data.map((genre) => (
+          <ListItem key={genre.id} paddingY='5px'>
+            <HStack>
+              <Image
+                boxSize='32px'
+                borderRadius={8}
+                objectFit='cover'
+                src={getCroppedImageUrl(genre.image_background)}
+              />
+              <Button
+                textAlign='left'
+                whiteSpace='normal'
+                fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'}
+                fontSize='lg'
+                variant='link'
+                onClick={() => {
+                  onSelectGenre(genre)
+                }}
+              >
+                {genre.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   )
 }
 export default GenreList
