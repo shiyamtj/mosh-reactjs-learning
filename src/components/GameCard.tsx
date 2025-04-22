402302c569c8903fc4c64225af90fde976556ccc
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-22 20:02:42 +1200

    add emojis

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,6 +3,7 @@ import { Game } from '../hooks/useGames'
 import PlatformIconList from './PlatformIconList'
 import CriticScore from './CriticScore'
 import getCroppedImageUrl from '../services/image-url'
+import Emoji from './Emoji'
 
 interface GameCardProps {
   game: Game
@@ -13,14 +14,15 @@ const GameCard = ({ game }: GameCardProps) => {
     <Card key={game.id}>
       <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize='2xl'>{game.name}</Heading>
-
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent='space-between' marginBottom={3}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize='2xl'>
+          {game.name} <Emoji rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,9 +19,9 @@ const GameGrid = ({ gameQuery }: GameGridProps) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing='10px'
-        padding={3}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        spacing={6}
+        padding='10px'
       >
         {isLoading &&
           skeletons.map((skeleton) => (
