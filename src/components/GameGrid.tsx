1ec8a83652409bdff7b106975a91c9c7599f9fa9
PC-SHIYAMJ\shiya <shiyamtj@gmail.com>
2025-04-22 20:13:09 +1200

    customized color

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,7 +3,7 @@ import useGames, { Platform } from '../hooks/useGames'
 import GameCard from './GameCard'
 import GameCardSkeleton from './GameCardSkeleton'
 import GameCardContainer from './GameCardContainer'
-import { Genre } from '../hooks/useGenre'
+// import { Genre } from '../hooks/useGenre'
 import { GameQuery } from '../App'
 
 interface GameGridProps {
